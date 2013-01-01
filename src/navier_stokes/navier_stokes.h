a3413ecb8913f52f0ce801c12f34877f07c08afd
Raymon White <raymonwhite42@gmail.com>
2013-01-01 01:20:42 +0000

    Autogen

--- src/navier_stokes/navier_stokes.h
+++ src/navier_stokes/navier_stokes.h
@@ -8,4 +8,5 @@
 #include<navier_stokes/navier_stokes_surface_power_elements.h> 
 #include<navier_stokes/navier_stokes_surface_drag_torque_elements.h> 
 #include<navier_stokes/navier_stokes_flux_control_elements.h> 
+#include<navier_stokes/constrained_navier_stokes_preconditioners.h> 
 #include<navier_stokes/impose_parallel_outflow_element.h> 
