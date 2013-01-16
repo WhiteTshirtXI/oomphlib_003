cb6860bfdb3168c6c55904a3f7674042f461f633
Raymon White <raymonwhite42@gmail.com>
2013-01-16 21:52:13 +0000

    Generalised the open file script

--- user_drivers/lagrange_square/open.sh
+++ user_drivers/lagrange_square/open.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-OOMPH_DIR="/learning/phd/wulfling/oomphlib_current"
+
+RAYTARGET=$HOSTNAME # cake, rice
+
+if [ "$RAYTARGET" == "cake" ]; then
+OOMPH_DIR="oomphlib/oomphlib_current"
 WORKING_DIR=$OOMPH_DIR"/user_drivers/lagrange_square"
 
 SRC_NS_DIR=$OOMPH_DIR"/src/navier_stokes"
 SRC_GEN_DIR=$OOMPH_DIR"/src/generic"
-
 
 gvim -p \
   ~/$WORKING_DIR/square0.cc \
@@ -13,6 +16,14 @@ gvim -p \
   ~/$SRC_NS_DIR/constrained_navier_stokes_preconditioners.cc \
   ~/$SRC_GEN_DIR/block_preconditioner.h \
   ~/$SRC_GEN_DIR/block_preconditioner.cc
-
+elif [ "$RAYTARGET" == rice ]; then
+  echo "Not done for $RAYTARGET"
+  read -p "Press [Enter] key to quit."
+  exit
+else
+  echo "No such target for $RAYTARGET"
+  read -p "Press [Enter] key to quit."
+  exit
+fi
 
 
